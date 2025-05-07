4a27b92d8e3758ac00d178676f4e56203767c828
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-07 11:54:56 +0200

    crear producto en Main

--- src/main/java/com/certidevs/Main.java
+++ src/main/java/com/certidevs/Main.java
@@ -1,13 +1,33 @@
 package com.certidevs;
 
+
+import com.certidevs.entities.Producto;
+import com.certidevs.repositories.ProductoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    public static void main(String[] args) {
 
+        // inicializar spring
+		ApplicationContext spring = SpringApplication.run(Main.class, args);
+
+        // obtener repositorio (lo crea spring)
+        ProductoRepository productoRepository = spring.getBean(ProductoRepository.class);
+
+        // crear productos
+        Producto manzana = new Producto("Manzana", 1.50, 100, true);
+        Producto yogur = new Producto("Yogur", 2.00, 50, true);
+        Producto avena = new Producto("Avena", 3.50, 30, true);
+
+        // guardar productos
+        productoRepository.save(manzana);
+        productoRepository.save(yogur);
+        productoRepository.save(avena);
+
+
+    }
 }
